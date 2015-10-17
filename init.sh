908815e628934bd71948c89cf046e349dea66b7b
Uldis Sturms <uldis.sturms@gmail.com>
2015-10-18 00:01:35 +0100

    Add zsh config

--- init.sh
+++ init.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
+# init vim
 git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 ln -sv ~/.dotfiles/.vimrc ~
 vim +PluginInstall +qall
+
+# init zsh
+ln -sv ~/.dotfiles/.zshrc ~
+sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"

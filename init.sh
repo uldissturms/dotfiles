af58f643ce9a7acb4f31124d0cda3ae4eb460230
Uldis Sturms <uldis@uldiss-macbook.cust.hyperoptic.com>
2015-10-27 01:13:23 +0000

    Link .zshrc file after oh-my-zsh is installed

--- init.sh
+++ init.sh
@@ -6,6 +6,6 @@ ln -sv ~/.dotfiles/.vimrc ~
 vim +PluginInstall +qall
 
 # init zsh
-ln -sv ~/.dotfiles/.zshrc ~
 sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 curl -o ~/.oh-my-zsh/themes/gitster.zsh-theme https://raw.githubusercontent.com/shashankmehta/dotfiles/master/thesetup/zsh/.oh-my-zsh/custom/themes/gitster.zsh-theme
+ln -sv ~/.dotfiles/.zshrc ~

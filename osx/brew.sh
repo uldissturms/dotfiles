1f55fa260f07cd257059674623257d2073802bff
Uldis Sturms <uldis.sturms@gmail.com>
2016-04-26 11:27:14 +0100

    brew install script

---
+++ osx/brew.sh
@@ -0,0 +1,13 @@
+brew install ab
+brew install docker-machine
+brew install jq
+brew install go
+brew install graphviz
+brew install highlight
+brew install lynx
+brew install node
+brew install tree
+brew install wget
+brew install watch
+brew install terraform
+brew install mono

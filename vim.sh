b75fafd24f6d256268cc195c453926ead89ea51d
Uldis Sturms <uldis.sturms@gmail.com>
2018-04-03 19:49:41 +0100

    fix(eslint): add required peer deps for standard/semistandard

--- vim.sh
+++ vim.sh
@@ -2,8 +2,10 @@
 npm i eslint \
   standard \
   semistandard \
+  eslint-plugin-node \
   eslint-plugin-import \
   eslint-plugin-standard \
+  eslint-config-standard \
   eslint-config-semistandard \
   eslint-plugin-promise \
   eslint-plugin-react -g

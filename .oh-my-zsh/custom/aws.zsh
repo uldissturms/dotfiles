8734f01425b1da16c9cedc65c6b40c97d70c08e8
Uldis Sturms <uldis.sturms@gmail.com>
2018-09-28 08:17:35 +0100

    chore: unset aws session token

--- .oh-my-zsh/custom/aws.zsh
+++ .oh-my-zsh/custom/aws.zsh
@@ -5,3 +5,9 @@ new-aws-session-token() {
   export AWS_SECRET_ACCESS_KEY=$(echo $creds | jq -r '.SecretAccessKey')
   export AWS_SESSION_TOKEN=$(echo $creds | jq -r '.SessionToken')
 }
+
+unset-aws-session-token() {
+  unset AWS_ACCESS_KEY_ID
+  unset AWS_SECRET_ACCESS_KEY
+  unset AWS_SESSION_TOKEN
+}

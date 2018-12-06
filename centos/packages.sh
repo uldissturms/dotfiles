fb959657c952d6701d4ee076d1b258d09e9faf11
Uldis Sturms <uldis.sturms@gmail.com>
2018-12-06 10:54:40 +0000

    chore(tmux): add centos deps for tmux

--- centos/packages.sh
+++ centos/packages.sh
@@ -4,12 +4,13 @@ curl --silent --location https://dl.yarnpkg.com/rpm/yarn.repo | sudo tee /etc/yu
 curl -fsSL https://get.docker.com -o get-docker.sh
 sudo sh get-docker.sh
 # go
-sudo yum install go
+sudo yum install go -y
 # fzf
 go get -u github.com/junegunn/fzf
 # git
-sudo yum install git-gui
+sudo yum install git-gui -y
 # rust
 curl https://sh.rustup.rs -sSf | sh
 # ag
-sudo yum install the_silver_searcher
+sudo yum install the_silver_searcher -y
+sudo yum install libevent-devel ncurses-devel glibc-static -y

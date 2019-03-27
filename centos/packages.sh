67a85e766897d1bdb8e56dee0d09fdb2706fae48
Uldis Sturms <uldis.sturms@gmail.com>
2019-03-27 13:43:13 +0000

    chore(centos): install gnome terminal

--- centos/packages.sh
+++ centos/packages.sh
@@ -19,3 +19,4 @@ sudo yum install libevent-devel ncurses-devel glibc-static -y
 sudo rpm --import https://packages.microsoft.com/keys/microsoft.asc
 sudo sh -c 'echo -e "[code]\nname=Visual Studio Code\nbaseurl=https://packages.microsoft.com/yumrepos/vscode\nenabled=1\ngpgcheck=1\ngpgkey=https://packages.microsoft.com/keys/microsoft.asc" > /etc/yum.repos.d/vscode.repo'
 sudo yum install code -y
+sudo yum install gnome-terminal -y

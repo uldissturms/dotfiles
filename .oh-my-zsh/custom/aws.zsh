96450fe43ccbbe0b3867f63b93e91ce71646fa57
Uldis Sturms <uldis.sturms@gmail.com>
2019-04-02 11:32:57 +0100

    chore(aws): add convenience gateway key function

--- .oh-my-zsh/custom/aws.zsh
+++ .oh-my-zsh/custom/aws.zsh
@@ -1,4 +1,4 @@
-new-aws-session-token() {
+aws-new-session-token() {
   creds=$(aws sts get-session-token | jq '.Credentials')
 
   export AWS_ACCESS_KEY_ID=$(echo $creds | jq -r '.AccessKeyId')
@@ -6,8 +6,12 @@ new-aws-session-token() {
   export AWS_SESSION_TOKEN=$(echo $creds | jq -r '.SessionToken')
 }
 
-unset-aws-session-token() {
+aws-unset-session-token() {
   unset AWS_ACCESS_KEY_ID
   unset AWS_SECRET_ACCESS_KEY
   unset AWS_SESSION_TOKEN
+}
+
+aws-gateway-key() {
+  aws apigateway get-api-keys --name-query $1 --include-value --query 'items[0].value' --output text
 }

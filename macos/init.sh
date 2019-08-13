6706254e62ea3dc07cae6e9e6a3865b2aebbecfd
Uldis Sturms <uldis.sturms@gmail.com>
2019-08-13 14:37:44 +0100

    chore: minor vs code tweaks

---
+++ macos/init.sh
@@ -0,0 +1,1 @@
+defaults write com.microsoft.VSCode ApplePressAndHoldEnabled -bool false # disable Press&Hold - needed for vscode VIM extension
--- vscode/init.sh
+++ vscode/init.sh
@@ -7,7 +7,6 @@ code --install-extension EditorConfig.EditorConfig
 code --install-extension kumar-harsh.graphql-for-vscode
 code --install-extension vscodevim.vim
 code --install-extension wayou.vscode-todo-highlight
-code --install-extension WallabyJs.quokka-vscode
 code --install-extension wmaurer.change-case
 code --install-extension akamud.vscode-theme-onedark
 code --install-extension ms-vscode.vscode-typescript-tslint-plugin

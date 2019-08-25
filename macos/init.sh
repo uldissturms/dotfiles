5577ea767ded3dac34c3922b801bb84ff49631ca
Uldis Sturms <uldis.sturms@gmail.com>
2019-08-25 17:54:23 +0100

    chore(macos): disable font smoothing

--- macos/init.sh
+++ macos/init.sh
@@ -1,1 +1,2 @@
 defaults write com.microsoft.VSCode ApplePressAndHoldEnabled -bool false # disable Press&Hold - needed for vscode VIM extension
+defaults write -g CGFontRenderingFontSmoothingDisabled -bool NO # switch off font smoothing - should fix blurry fonts

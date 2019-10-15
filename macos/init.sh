b5653fa83807010f4cced980ed0bfc63a0390afe
Uldis Sturms <uldis.sturms@gmail.com>
2019-10-15 16:33:59 +0100

    chore(mac): reference on CLI command to install mac command line tools

--- macos/init.sh
+++ macos/init.sh
@@ -1,2 +1,5 @@
 defaults write com.microsoft.VSCode ApplePressAndHoldEnabled -bool false # disable Press&Hold - needed for vscode VIM extension
 defaults write -g CGFontRenderingFontSmoothingDisabled -bool NO # switch off font smoothing - should fix blurry fonts
+
+# install command line tools
+# xcode-select --install
